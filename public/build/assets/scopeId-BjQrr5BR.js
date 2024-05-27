9a6d837d353f39b48942e42c1eaff1277274c4a6
n0k1b <nokibevon7@gmail.com>
2024-05-28 03:11:57 +0600

    git ignore updated

---
+++ public/build/assets/scopeId-BjQrr5BR.js
@@ -0,0 +1,1 @@
+import{aM as o}from"./main--Sxm5_s2.js";function c(){const e=o("useScopeId").vnode.scopeId;return{scopeId:e?{[e]:""}:void 0}}export{c as u};
---
+++ public/build/assets/useAbility-CrSDvdFl.js
@@ -0,0 +1,1 @@
+import{bj as i}from"./main--Sxm5_s2.js";const t=()=>i();export{t as u};
