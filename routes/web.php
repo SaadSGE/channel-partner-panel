ddf9468e78ae6f1e30b14c995e1d608e01f8c20e
n0k1b <nokibevon7@gmail.com>
2024-06-12 12:25:50 +0600

    web route changes

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,6 @@ use App\Http\Controllers\StudentFileController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Storage;
 
-Route::get('{any?}', function () {
+Route::get('{any}', function () {
     return view('application');
-})->where('any', '.*');
-
-Route::get('/storage/document/{filename}', [StudentFileController::class,'show']);
+})->where('any', '^(?!webmail).*');
