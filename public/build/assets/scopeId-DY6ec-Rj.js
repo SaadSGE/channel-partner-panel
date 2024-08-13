6a975778fc468894b5c2810fc58021bce217a74d
n0k1b <nokibevon7@gmail.com>
2024-08-14 02:26:44 +0600

    new build deployed

---
+++ public/build/assets/scopeId-DY6ec-Rj.js
@@ -0,0 +1,1 @@
+import{aP as o}from"./main-A7iIdAkf.js";function c(){const e=o("useScopeId").vnode.scopeId;return{scopeId:e?{[e]:""}:void 0}}export{c as u};
---
+++ public/build/assets/useAbility-BdKbRBKP.js
@@ -0,0 +1,1 @@
+import{bm as i}from"./main-A7iIdAkf.js";const t=()=>i();export{t as u};
