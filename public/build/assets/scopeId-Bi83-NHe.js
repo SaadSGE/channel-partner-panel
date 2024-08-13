cd04795f76c90b58aad4e1f76b353919f01f8aca
n0k1b <nokibevon7@gmail.com>
2024-08-14 01:54:44 +0600

    new build deployed

--- public/build/assets/scopeId-Bi83-NHe.js
+++
@@ -1,1 +0,0 @@
-import{aP as o}from"./main-ggi0n7Qr.js";function c(){const e=o("useScopeId").vnode.scopeId;return{scopeId:e?{[e]:""}:void 0}}export{c as u};
--- public/build/assets/useAbility-BA8cmAoo.js
+++
@@ -1,1 +0,0 @@
-import{bm as i}from"./main-ggi0n7Qr.js";const t=()=>i();export{t as u};
