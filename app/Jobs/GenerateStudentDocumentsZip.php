7749a294fb311c8e57e1799264998d72c7c6c3d8
n0k1b <nokibevon7@gmail.com>
2024-08-19 18:44:03 +0600

    file url log

--- app/Jobs/GenerateStudentDocumentsZip.php
+++ app/Jobs/GenerateStudentDocumentsZip.php
@@ -58,6 +58,7 @@ class GenerateStudentDocumentsZip implements ShouldQueue
 
             foreach ($documents as $document) {
                 $fileUrl = $document->original_path;
+                \Log::info($fileUrl);
                 $fileName = basename($fileUrl);
 
                 $response = $client->get($fileUrl);
