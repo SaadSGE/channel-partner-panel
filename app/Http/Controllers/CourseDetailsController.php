ce6e5f0b7b7c086470a36b4c3559939573954804
n0k1b <nokibevon7@gmail.com>
2024-09-03 03:13:44 +0600

    course details cachec remove

--- app/Http/Controllers/CourseDetailsController.php
+++ app/Http/Controllers/CourseDetailsController.php
@@ -215,7 +215,7 @@ class CourseDetailsController extends Controller
 
     public function courseDetailsAll()
     {
-        Cache::forget('course_details_all');
+
         // Define a cache key
         $cacheKey = 'course_details_all';
 
