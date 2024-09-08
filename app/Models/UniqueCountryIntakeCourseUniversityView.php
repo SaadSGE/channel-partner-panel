aa42e500d4ef3ccee2fa4b06d4f654f1a01e61bd
n0k1b <nokibevon7@gmail.com>
2024-09-08 20:41:48 +0600

    university logo url fixed

--- app/Models/UniqueCountryIntakeCourseUniversityView.php
+++ app/Models/UniqueCountryIntakeCourseUniversityView.php
@@ -9,4 +9,15 @@ class UniqueCountryIntakeCourseUniversityView extends Model
 {
     use HasFactory;
     protected $table = "unique_country_intake_course_university_view";
+
+
+    public function getUniversityLogoAttribute($value)
+    {
+        if (!$value) {
+            return null;
+        }
+
+        $doUrl = config('filesystems.disks.do.url', env('DO_URL'));
+        return $doUrl . $value;
+    }
 }
