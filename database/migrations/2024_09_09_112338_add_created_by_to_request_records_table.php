f635ed657e0e96eb8c8b3abfde7f72892cdf11a8
n0k1b <nokibevon7@gmail.com>
2024-09-15 12:26:16 +0600

    request record table added

--- database/migrations/2024_09_09_112338_add_created_by_to_request_records_table.php
+++ database/migrations/2024_09_09_112338_add_created_by_to_request_records_table.php
@@ -11,7 +11,7 @@ return new class () extends Migration {
     public function up(): void
     {
         Schema::table('request_records', function (Blueprint $table) {
-
+            $table->unsignedBigInteger('created_by')->nullable()->after('id');
             $table->integer('status')->default(0)->after('created_by');
 
         });
