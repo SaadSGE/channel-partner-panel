011bcb929d56c8211d0f9aab397fafeb6ae40b50
n0k1b <nokibevon7@gmail.com>
2024-09-18 18:25:42 +0600

    new build deployed

--- public/build/assets/VCardText-DwoObMNb.js
+++
@@ -1,1 +0,0 @@
-import{c as t}from"./VAvatar-Tgh28dnY.js";const e=t("v-card-text");export{e as V};
--- public/build/assets/VSpacer-DdxwTxzT.js
+++
@@ -1,1 +0,0 @@
-/* empty css              */import{c as r}from"./VAvatar-Tgh28dnY.js";const e=r("v-spacer","div","VSpacer");export{e as V};
