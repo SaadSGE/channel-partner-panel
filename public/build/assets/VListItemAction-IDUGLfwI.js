28fe281fcd7f31a80d22073320eba6be50656ba1
n0k1b <nokibevon7@gmail.com>
2024-10-05 20:42:53 +0600

    build file remove temporary

--- public/build/assets/VListItemAction-IDUGLfwI.js
+++
@@ -1,1 +0,0 @@
-import{aL as s,aM as o,aN as n,aO as i,aU as m,b as c}from"./main-ofHzgIAY.js";const r=s({start:Boolean,end:Boolean,...o(),...n()},"VListItemAction"),V=i()({name:"VListItemAction",props:r(),setup(t,e){let{slots:a}=e;return m(()=>c(t.tag,{class:["v-list-item-action",{"v-list-item-action--start":t.start,"v-list-item-action--end":t.end},t.class],style:t.style},a)),{}}});export{V};
--- public/build/assets/useAbility-Bqg4zWan.js
+++
@@ -1,1 +0,0 @@
-import{bn as i}from"./main-ofHzgIAY.js";const t=()=>i();export{t as u};
