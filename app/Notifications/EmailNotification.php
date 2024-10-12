f57b9baab35ee690d7ffef8cbe601eee26204662
n0k1b <nokibevon7@gmail.com>
2024-10-12 13:09:32 +0600

    email notification error fixed

--- app/Notifications/EmailNotification.php
+++ app/Notifications/EmailNotification.php
@@ -61,7 +61,7 @@ class EmailNotification extends Notification implements ShouldQueue
             'sender_name' => $this->details['sender_name'],  // Include sender_name
             'sender_email' => $this->details['sender_email'],  // Include sender_email
             'notification_type' => $this->details['notification_type'],
-            'notification_text' => $this->details['notification_text'] ?? 'A new course/university/intake request has been submitted',
+            'notification_text' => $this->details['notification_text'] ?? '',
             'notification_route' => $this->details['notification_route'] ?? '',
 
         ];
