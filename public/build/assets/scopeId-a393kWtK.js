11ff222b6cf7ace2f1a7a66649bcc3eff52f9b80
n0k1b <nokibevon7@gmail.com>
2024-10-18 15:32:32 +0600

    new build deployed

--- public/build/assets/scopeId-a393kWtK.js
+++
@@ -1,1 +0,0 @@
-import{aP as o}from"./main-DNPt8sB9.js";function c(){const e=o("useScopeId").vnode.scopeId;return{scopeId:e?{[e]:""}:void 0}}export{c as u};
--- public/build/assets/useAbility-C6m7C5En.js
+++
@@ -1,1 +0,0 @@
-import{bm as i}from"./main-DNPt8sB9.js";const t=()=>i();export{t as u};
