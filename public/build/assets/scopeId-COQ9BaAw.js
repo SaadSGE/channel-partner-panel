47e13a15c1a158265c83fc04a1642ec908900e98
n0k1b <nokibevon7@gmail.com>
2024-10-19 03:20:29 +0600

    build file removed

--- public/build/assets/scopeId-COQ9BaAw.js
+++
@@ -1,1 +0,0 @@
-import{b6 as o}from"./main-Cp2POdXb.js";function c(){const e=o("useScopeId").vnode.scopeId;return{scopeId:e?{[e]:""}:void 0}}export{c as u};
--- public/build/assets/useAbility-BlVrxauM.js
+++
@@ -1,1 +0,0 @@
-import{bm as i}from"./main-Cp2POdXb.js";const t=()=>i();export{t as u};
