973cac6cbc6cb91ece0092def152ec37439c4c06
n0k1b <nokibevon7@gmail.com>
2024-11-06 22:19:08 +0600

    new build deployed

--- public/build/assets/VSpacer-DSg4sib0.js
+++
@@ -1,1 +0,0 @@
-/* empty css              */import{c as r}from"./VAvatar-CO7KAs2D.js";const e=r("v-spacer","div","VSpacer");export{e as V};
---
+++ public/build/assets/useAbility-CHggFHm8.js
@@ -0,0 +1,1 @@
+import{bm as i}from"./main-C5iX8-Hn.js";const t=()=>i();export{t as u};
