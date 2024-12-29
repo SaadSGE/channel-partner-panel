998f6a861323646e18056c62fd0298e4deb05ef3
n0k1b <nokibevon7@gmail.com>
2024-12-30 03:45:11 +0600

    new build deployed

--- public/build/assets/VCardText-D80AumUZ.js
+++
@@ -1,1 +0,0 @@
-import{c as t}from"./VAvatar-C8jnXoKy.js";const e=t("v-card-text");export{e as V};
---
+++ public/build/assets/VNodeRenderer-pRvu49i9.js
@@ -0,0 +1,1 @@
+import{M as r}from"./main-Chpg_S2F.js";const n=r({name:"VNodeRenderer",props:{nodes:{type:[Array,Object],required:!0}},setup(e){return()=>e.nodes}});export{n as V};
