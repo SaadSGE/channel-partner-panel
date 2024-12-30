c05d8b5bf724302695445b0f95f8b75156bc384a
n0k1b <nokibevon7@gmail.com>
2024-12-30 18:23:44 +0600

    Lead controller updated

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Lead;
+use App\Models\lead as Lead;
 use Maatwebsite\Excel\Facades\Excel;
 use App\Imports\LeadsImport;
 use Illuminate\Validation\ValidationException;
