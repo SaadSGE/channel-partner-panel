cedebf46332a34dce810afaf6d42b7915a3dbfa3
n0k1b <nokibevon7@gmail.com>
2025-01-01 22:21:00 +0600

    new build deployed

---
+++ public/build/assets/VNodeRenderer-4kv-5NwM.js
@@ -0,0 +1,1 @@
+import{M as r}from"./main-CcBrA5VQ.js";const n=r({name:"VNodeRenderer",props:{nodes:{type:[Array,Object],required:!0}},setup(e){return()=>e.nodes}});export{n as V};
--- public/build/assets/useAbility-DAmQBD7s.js
+++
@@ -1,1 +0,0 @@
-import{b7 as i}from"./main-vWBHgCBI.js";const t=()=>i();export{t as u};
