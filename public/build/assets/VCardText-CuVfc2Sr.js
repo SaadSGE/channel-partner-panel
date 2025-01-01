f750e6d5317c8b854aaf259976ca7bd74701b49f
n0k1b <nokibevon7@gmail.com>
2025-01-01 20:20:49 +0600

    new build deployed

--- public/build/assets/VCardText-CuVfc2Sr.js
+++
@@ -1,1 +0,0 @@
-import{c as t}from"./VAvatar-C7sb39K_.js";const e=t("v-card-text");export{e as V};
--- public/build/assets/VSpacer-5qrbgCEH.js
+++
@@ -1,1 +0,0 @@
-/* empty css              */import{c as r}from"./VAvatar-C7sb39K_.js";const e=r("v-spacer","div","VSpacer");export{e as V};
