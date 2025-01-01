339bb91c674f8da3a68f4992ee6720a1a6689b6e
n0k1b <nokibevon7@gmail.com>
2025-01-01 20:26:24 +0600

    Daily task disable for non-admin users

--- resources/js/@core/components/cards/AddDailyTask.vue
+++ resources/js/@core/components/cards/AddDailyTask.vue
@@ -8,79 +8,79 @@
 // })
 </script>
 
-<template>
-    <a class="daily-task-button d-print-none" role="button" rel="noopener noreferrer" target="_blank">
-        Add Daily Task
-        <span class="button-inner" />
-    </a>
+<template v-if="$can('read', 'daily-task')">
+  <a class="daily-task-button d-print-none" role="button" rel="noopener noreferrer" target="_blank">
+    Add Daily Task
+    <span class="button-inner" />
+  </a>
 </template>
 
 <style lang="scss" scoped>
 .daily-task-button,
 .button-inner {
-    display: inline-flex;
-    box-sizing: border-box;
-    align-items: center;
-    justify-content: center;
-    border: 0;
-    border-radius: 6px;
-    animation: anime 12s linear infinite;
-    appearance: none;
-    background: linear-gradient(-45deg, #ffa63d, #ff3d77, #338aff, #3cf0c5);
-    background-size: 600%;
-    color: rgba(255, 255, 255, 90%);
-    cursor: pointer;
-    font-size: 0.9375rem;
-    font-weight: 500;
-    letter-spacing: 0.43px;
-    line-height: 1.2;
-    margin-inline-end: 20px;
-    min-inline-size: 50px;
-    outline: 0;
-    padding-block: 0.625rem;
-    padding-inline: 1.25rem;
-    text-decoration: none;
-    text-transform: none;
-    vertical-align: middle;
+  display: inline-flex;
+  box-sizing: border-box;
+  align-items: center;
+  justify-content: center;
+  border: 0;
+  border-radius: 6px;
+  animation: anime 12s linear infinite;
+  appearance: none;
+  background: linear-gradient(-45deg, #ffa63d, #ff3d77, #338aff, #3cf0c5);
+  background-size: 600%;
+  color: rgba(255, 255, 255, 90%);
+  cursor: pointer;
+  font-size: 0.9375rem;
+  font-weight: 500;
+  letter-spacing: 0.43px;
+  line-height: 1.2;
+  margin-inline-end: 20px;
+  min-inline-size: 50px;
+  outline: 0;
+  padding-block: 0.625rem;
+  padding-inline: 1.25rem;
+  text-decoration: none;
+  text-transform: none;
+  vertical-align: middle;
 }
 
 .daily-task-button {
-    position: fixed;
-    z-index: 999;
-    inset-block-start: 50%;
-    inset-inline-end: 87px;
-    transform: translateY(-50%);
+  position: fixed;
+  z-index: 999;
+  inset-block-start: 50%;
+  inset-inline-end: 87px;
+  transform: translateY(-50%);
 
-    &:hover {
-        color: white;
-        text-decoration: none;
-    }
+  &:hover {
+    color: white;
+    text-decoration: none;
+  }
 
-    .button-inner {
-        position: absolute;
-        z-index: -1;
-        filter: blur(12px);
-        inset: 0;
-        opacity: 0;
-        transition: opacity 200ms ease-in-out;
-    }
+  .button-inner {
+    position: absolute;
+    z-index: -1;
+    filter: blur(12px);
+    inset: 0;
+    opacity: 0;
+    transition: opacity 200ms ease-in-out;
+  }
 
-    &:not(:hover) .button-inner {
-        opacity: 0.8;
-    }
+  &:not(:hover) .button-inner {
+    opacity: 0.8;
+  }
 }
 
 @keyframes anime {
-    0% {
-        background-position: 0% 50%;
-    }
+  0% {
+    background-position: 0% 50%;
+  }
 
-    50% {
-        background-position: 100% 50%;
-    }
+  50% {
+    background-position: 100% 50%;
+  }
 
-    100% {
-        background-position: 0% 50%;
-    }
+  100% {
+    background-position: 0% 50%;
+  }
 }
 </style>
